566105e0327b484092963caee8de2629eeb81fa0
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-03 18:23:38 -0400

    finishing mask effect when moving cursor

--- src/app/page.js
+++ src/app/page.js
@@ -2,26 +2,31 @@
 import useMousePosition from './utils/useMousePosition'
 import styles from './page.module.scss'
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export default function Home() {
 
-  const {x, y} = useMousePosition();
-  const size = 40;
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered? 400 : 40; //changing mask size if hovered over text
   return (
     <main className={styles.main}>
 
-      <motion.div 
+      <motion.div
         className={styles.mask}
         animate={{
-          WebkitMaskPosition: `${x - size/2}px ${y - size/2}px`,
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`
         }}
-        transition={{type: "tween", ease: "backOut"}}
-        >
-        <p>This is Testing Testing Testing Testing Testing Mask Text</p>
+        transition={{ type: "tween", ease: "backOut" }}
+      >
+        <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..
+        </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p>This is Different Different Different Different Different <span>Body</span> Text</p>
+        <p>This is Different Different Different Different<span> Different Body</span> Text</p>
       </div>
 
     </main>
