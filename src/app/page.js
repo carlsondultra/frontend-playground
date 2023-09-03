6a3838e13d4d22d839dbd8257c7af66b453c9962
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-03 18:11:24 -0400

    adding movable cursor using motion div

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
 'use client'
 import useMousePosition from './utils/useMousePosition'
 import styles from './page.module.scss'
+import { motion } from 'framer-motion';
 
 export default function Home() {
 
-  const {x, y} = useMousePosition;
+  const {x, y} = useMousePosition();
+  const size = 40;
   return (
     <main className={styles.main}>
 
-      <div className={styles.mask}>
+      <motion.div 
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - size/2}px ${y - size/2}px`,
+        }}
+        transition={{type: "tween", ease: "backOut"}}
+        >
         <p>This is Testing Testing Testing Testing Testing Mask Text</p>
-      </div>
+      </motion.div>
 
       <div className={styles.body}>
         <p>This is Different Different Different Different Different <span>Body</span> Text</p>
