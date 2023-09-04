be992ccbf209ba7a1e039d2acfbca5a9cb06b2cb
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-04 15:59:34 -0400

    using next link to link between homepage and mask effect

---
+++ src/app/maskeffect/page.js
@@ -0,0 +1,34 @@
+'use client'
+import useMousePosition from './utils/useMousePosition'
+import styles from '../page.module.scss'
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+export default function Home() {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered? 400 : 40; //changing mask size if hovered over text
+  return (
+    <main className={styles.main}>
+
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`
+        }}
+        transition={{ type: "tween", ease: "backOut" }}
+      >
+        <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..
+        </p>
+      </motion.div>
+
+      <div className={styles.body}>
+        <p>This is Different Different Different Different<span> Different Body</span> Text</p>
+      </div>
+
+    </main>
+  )
+}
