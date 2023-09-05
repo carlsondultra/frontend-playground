0a19aee045e7011d5c4d7b7f6dc8550f19315e1a
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-05 18:09:23 -0400

    adding images to page

---
+++ src/app/imagefloat/data.js
@@ -0,0 +1,19 @@
+import Floating1 from "../../../public/imagefloat/floating_1.jpg"
+import Floating2 from "../../../public/imagefloat/floating_2.jpg"
+import Floating3 from "../../../public/imagefloat/floating_3.jpg"
+import Floating4 from "../../../public/imagefloat/floating_4.jpg"
+import Floating5 from "../../../public/imagefloat/floating_5.jpg"
+import Floating6 from "../../../public/imagefloat/floating_6.jpg"
+import Floating7 from "../../../public/imagefloat/floating_7.jpg"
+import Floating8 from "../../../public/imagefloat/floating_8.jpg"
+
+export {
+    Floating1,
+    Floating2,
+    Floating3,
+    Floating4,
+    Floating5,
+    Floating6,
+    Floating7,
+    Floating8,
+}
--- src/app/imagefloat/page.js
+++ src/app/imagefloat/page.js
@@ -1,8 +1,67 @@
+import {
+    Floating1,
+    Floating2,
+    Floating3,
+    Floating4,
+    Floating5,
+    Floating6,
+    Floating7,
+    Floating8
+} from "../imagefloat/data"
+import styles from "./page.module.scss"
+import Image from "next/image"
+
+
 
 export default function Home() {
     return(
-        <div>
-            Testing
-        </div>
+        <main className={styles.main}>
+            <div className={styles.plane}>
+                <Image
+                    alt="image"
+                    src={Floating1}
+                    width={300}
+                />
+                <Image
+                    alt="image"
+                    src={Floating2}
+                    width={300}
+                />
+                <Image
+                    alt="image"
+                    src={Floating3}
+                    width={300}
+                />
+            </div>
+            <div className={styles.plane}>
+                <Image
+                    alt="image"
+                    src={Floating4}
+                    width={300}
+                />
+                <Image
+                    alt="image"
+                    src={Floating5}
+                    width={300}
+                />
+                <Image
+                    alt="image"
+                    src={Floating6}
+                    width={300}
+                />
+            </div>
+            <div className={styles.plane}>
+                <Image
+                    alt="image"
+                    src={Floating7}
+                    width={300}
+                />
+                <Image
+                    alt="image"
+                    src={Floating8}
+                    width={300}
+                />
+            </div>
+        </main>
     )
 }
