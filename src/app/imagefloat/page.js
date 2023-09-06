4cbabdbd8c1aa3dc37e93416cf72bc94c11e9ade
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-05 21:45:00 -0400

    adding ability to cancel request animation frame

--- src/app/imagefloat/page.js
+++ src/app/imagefloat/page.js
@@ -31,7 +31,7 @@ export default function Home() {
         xForce += movementX * speed;
         yForce += movementY * speed;
 
-        if(!requestAnimationFrameId){
+        if (!requestAnimationFrameId) {
             requestAnimationFrameId = requestAnimationFrame(animate);
         }
     }
@@ -43,14 +43,28 @@ export default function Home() {
     const animate = () => {
         xForce = lerp(xForce, 0, easing)
         yForce = lerp(yForce, 0, easing)
-        gsap.set(plane1.current, {x: `+=${xForce}`,y: `+=${yForce}`})
-        gsap.set(plane2.current, {x: `+=${xForce * 0.5}`,y: `+=${yForce * 0.5}`})
-        gsap.set(plane3.current, {x: `+=${xForce * 0.25}`,y: `+=${yForce * 0.25}`})
+        gsap.set(plane1.current, { x: `+=${xForce}`, y: `+=${yForce}` })
+        gsap.set(plane2.current, { x: `+=${xForce * 0.5}`, y: `+=${yForce * 0.5}` })
+        gsap.set(plane3.current, { x: `+=${xForce * 0.25}`, y: `+=${yForce * 0.25}` })
         requestAnimationFrame(animate)
+ 
+        // cancelling request animation frame
+        if (Math.abs(xForce) < 0.01) xForce = 0;
+        if (Math.abs(yForce) < 0.01) yForce = 0;
+
+        if (xForce > 0 || yForce > 0) {
+            requestAnimationFrame(animate);
+        }
+        else {
+            cancelAnimationFrame(requestAnimationFrameId)
+            requestAnimationFrameId = null;
+        }
+
+
     }
 
     return (
-        <main onMouseMove={(e) => {manageMouseMove(e)}} className={styles.main}>
+        <main onMouseMove={(e) => { manageMouseMove(e) }} className={styles.main}>
             <div ref={plane1} className={styles.plane}>
                 <Image
                     alt="image"
