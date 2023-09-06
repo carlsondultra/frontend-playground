6be99bbb2cde4b6a5a5ff98b2f18962e44f32828
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-06 16:55:10 -0400

    adding moving effect on text using gsap

--- src/app/infinitetext/page.js
+++ src/app/infinitetext/page.js
@@ -1,14 +1,43 @@
+'use client'
 import Image from 'next/image'
-import styles from './page.module.scss'
+import styles from './page.module.css'
+import { gsap } from 'gsap'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
-  return (
-      <main>
-        <Image
-            fill={true}
-            src="/imagefloat/floating_5.jpg"
-            alt="image"
-        />
-      </main>   
-  )
+
+    const firstText = useRef(null)
+    const secondText = useRef(null)
+    let xPercent = 0;
+    let direction = -1;
+
+    useEffect(() => {
+        requestAnimationFrame(animation);
+    }, [])
+
+    // using gsap to set position of x based on xpercent value
+    const animation = () => {
+        gsap.set(firstText.current, {xPercent: xPercent})
+        gsap.set(secondText.current, {xPercent: xPercent})
+        xPercent += 0.1 * direction;
+        requestAnimationFrame(animation);
+    }
+
+    return (
+        <main>
+            <Image
+                fill={true}
+                src="/imagefloat/floating_5.jpg"
+                alt="image"
+            />
+            <div className={styles.sliderContainer}>
+                <div className={styles.slider}>
+                    {/* Repeated paragraphs in order to have infinite effect */}
+                    <p ref={firstText}>Created using Next.js and GSAP</p>
+                    <p ref={secondText}>Created using Next.js and GSAP</p>
+                </div>
+            </div>
+
+        </main>
+    )
 }
