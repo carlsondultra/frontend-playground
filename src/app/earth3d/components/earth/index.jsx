c72efe2a34f49101934cf00af2d656ef7b85a723
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-08 19:50:11 -0400

    rotating 3d earth when scrolling down

--- src/app/earth3d/components/earth/index.jsx
+++ src/app/earth3d/components/earth/index.jsx
@@ -1,8 +1,18 @@
 'use client';
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { useEffect, useRef } from "react";
+import { useScroll } from "framer-motion";
+import { motion } from 'framer-motion-3d';
 
 export default function earth() {
+
+    const scene = useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: scene,
+        offset: ['start end', 'end start']
+    })
 
     const [color, normal, aoMap] = useLoader(TextureLoader, [
         '/earth3d/color.jpg',
@@ -10,12 +20,13 @@ export default function earth() {
         '/earth3d/occlusion.jpg'
     ])
     return (
-        <Canvas>
-            <ambientLight intensity={2} />
-            <mesh scale={2.5}>
+        <Canvas ref={scene}>
+            <ambientLight intensity={0.1} />
+            <directionalLight intensity={3.5} position={[1, 0, -.25]} />
+            <motion.mesh scale={2.5} rotation-y={scrollYProgress}>
                 <sphereGeometry args={[1, 64, 64]} />
                 <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
-            </mesh>
+            </motion.mesh>
         </Canvas>
     )
 } 
