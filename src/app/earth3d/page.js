6a84fc3345062e14c2b32a27805e88a0f731c64a
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-08 19:53:33 -0400

    using lenis scroll for smooth animation scroll

--- src/app/earth3d/page.js
+++ src/app/earth3d/page.js
@@ -1,10 +1,14 @@
 import styles from './page.module.scss'
 import Earth from './components/earth'
+import SmoothScroll from './components/smoothScroll'
 
 export default function Home() {
     return (
+    <SmoothScroll>
         <main className={styles.main}>
             <Earth />
         </main>
+    </SmoothScroll>
+        
     )
 }
