66a54b8b15a55c34cee87b81dfd08b28ebbfb88f
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-08 18:43:08 -0400

    creating black and white earth

--- src/app/earth3d/components/earth/index.jsx
+++ src/app/earth3d/components/earth/index.jsx
@@ -1,12 +1,20 @@
 'use client';
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 export default function earth() {
+
+    const [color, normal, aoMap] = useLoader(TextureLoader, [
+        '/earth3d/color.jpg',
+        '/earth3d/normal.png',
+        '/earth3d/occlusion.jpg'
+    ])
     return (
         <Canvas>
             <ambientLight intensity={2} />
             <mesh scale={2.5}>
-                <sphereGeometry />
+                <sphereGeometry args={[1, 64, 64]} />
+                <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
             </mesh>
         </Canvas>
     )
