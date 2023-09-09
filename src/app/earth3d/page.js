d273f9e56a7a2a0c5318b0063d34fd995d44c957
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-08 20:02:23 -0400

    added projects with text color matching earth rotate

--- src/app/earth3d/page.js
+++ src/app/earth3d/page.js
@@ -1,12 +1,14 @@
 import styles from './page.module.scss'
 import Earth from './components/earth'
 import SmoothScroll from './components/smoothScroll'
+import Projects from './components/projects'
 
 export default function Home() {
     return (
     <SmoothScroll>
         <main className={styles.main}>
-            <Earth />
+            <Earth />\
+            <Projects />
         </main>
     </SmoothScroll>
         
