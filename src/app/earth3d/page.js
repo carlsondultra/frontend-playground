63c26b9fcf66cd2c870594c5a9dde95be7048f8a
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-08 20:13:26 -0400

    added project title

--- src/app/earth3d/page.js
+++ src/app/earth3d/page.js
@@ -7,16 +7,25 @@ import dynamic from 'next/dynamic'
 const Earth = dynamic(() => import('./components/earth'), {
     ssr: false,
     loading: () => <img src="/assets/placeholder.png"></img>
-  })
+})
 
 export default function Home() {
     return (
-    <SmoothScroll>
-        <main className={styles.main}>
-            <Earth />
-            <Projects />
-        </main>
-    </SmoothScroll>
-        
+        <SmoothScroll>
+            <div className={styles.spacer}>
+                <div className={styles.title}>
+                    <h1>3D Earth with Lenis Scroll</h1>
+                    <p>Created using Next.js, Framer Motion, Lenis Scroll, and React Three Fiber</p>
+                    <br></br>
+                    <p>Scroll down to see effect</p>
+                </div>
+
+            </div>
+            <main className={styles.main}>
+                <Earth />
+                <Projects />
+            </main>
+        </SmoothScroll>
+
     )
 }
