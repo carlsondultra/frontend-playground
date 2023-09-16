df20c04d304b3e18c0fd2f72d93ff1815c491dfe
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-16 17:54:11 -0400

    adding spacer and css

--- src/app/cube3d/page.js
+++ src/app/cube3d/page.js
@@ -1,11 +1,20 @@
-import styles from './page.module.scss'
 import Cube from './components/cube'
 import Navbar from '../components/navbar/navbar'
+import styles from './style.module.scss'
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <div className={styles.spacer}>
+        <Navbar />
+        <div className={styles.title}>
+          <h1>3D Cube with scroll</h1>
+          <p>Created using Next.js, Framer Motion and Three.js</p>
+          <br></br>
+          <p>Scroll down to see effect</p>
+        </div>
+
+      </div>
       <Cube />
     </main>
 
