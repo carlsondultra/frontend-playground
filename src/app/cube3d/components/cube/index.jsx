f8445b8a5ffaf0f2439d7b03a6b7ed4dab92a266
carlsondultra <carlsondultra@users.noreply.github.com>
2023-09-16 17:09:12 -0400

    adding ability to scroll and move cube

--- src/app/cube3d/components/cube/index.jsx
+++ src/app/cube3d/components/cube/index.jsx
@@ -5,7 +5,7 @@ import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import styles from './style.module.scss'
 import '../../styles.css'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, ScrollControls, useScroll } from '@react-three/drei'
 import { useMotionValue, useSpring } from 'framer-motion'
 import { motion } from 'framer-motion-3d'
 
@@ -13,11 +13,13 @@ export default function index() {
     return (
         <div className={styles.main}>
             <Canvas>
-                {/* disabling zooming and panning on cube */}
-                <OrbitControls enableZoom={false} enablePan={false}/>
-                <ambientLight intensity={2}/>
-                <directionalLight position={[2, 1, 1]} />
-                <Cube />
+                <ScrollControls pages={5}>
+                    {/* disabling zooming and panning on cube */}
+                    <OrbitControls enableZoom={false} enablePan={false} />
+                    <ambientLight intensity={2} />
+                    <directionalLight position={[2, 1, 1]} />
+                    <Cube />
+                </ScrollControls>
             </Canvas>
         </div>
     )
@@ -26,30 +28,38 @@ export default function index() {
 function Cube() {
 
     const mesh = useRef(null);
+    const data = useScroll();
 
-    const options = {
-        damping: 20
-    }
+    useFrame( (state, delta) => {
+        const { offset } = data;
+        mesh.current.rotation.x = offset * 5;
+        mesh.current.rotation.y = offset * 5;
+        mesh.current.rotation.z = offset * 5;
+    })
 
-    const mouse = {
-        x: useSpring(useMotionValue(0), options),
-        y: useSpring(useMotionValue(0), options)
-    }
+    // const options = {
+    //     damping: 20
+    // }
 
-    const manageMouseMove = (e) => {
-        const { innerWidth, innerHeight } = window;
-        const { clientX, clientY } = e;
-        const x = -0.5 + clientX / innerWidth
-        const y = -0.5 + clientY / innerHeight
-        mouse.x.set(x);
-        mouse.y.set(y)
-    }
+    // const mouse = {
+    //     x: useSpring(useMotionValue(0), options),
+    //     y: useSpring(useMotionValue(0), options)
+    // }
 
-    useEffect(() => {
-        window.addEventListener("mousemove", manageMouseMove)
+    // const manageMouseMove = (e) => {
+    //     const { innerWidth, innerHeight } = window;
+    //     const { clientX, clientY } = e;
+    //     const x = -0.5 + clientX / innerWidth
+    //     const y = -0.5 + clientY / innerHeight
+    //     mouse.x.set(x);
+    //     mouse.y.set(y)
+    // }
 
-        return () => window.removeEventListener("mouse", manageMouseMove)
-    }, [])
+    // useEffect(() => {
+    //     window.addEventListener("mousemove", manageMouseMove)
+
+    //     return () => window.removeEventListener("mouse", manageMouseMove)
+    // }, [])
 
     const texture_1 = useLoader(TextureLoader, "/imagefloat/floating_1.jpg")
     const texture_2 = useLoader(TextureLoader, "/imagefloat/floating_2.jpg")
@@ -59,14 +69,14 @@ function Cube() {
     const texture_6 = useLoader(TextureLoader, "/imagefloat/floating_6.jpg")
 
     return (
-        <motion.mesh ref={mesh} rotation-x={mouse.y} rotation-y={mouse.x}>
-            <boxGeometry args={[2.5, 2.5, 2.5]}/>
-            <meshStandardMaterial map={texture_1} attach="material-0"/>
-            <meshStandardMaterial map={texture_2} attach="material-1"/>
-            <meshStandardMaterial map={texture_3} attach="material-2"/>
-            <meshStandardMaterial map={texture_4} attach="material-3"/>
-            <meshStandardMaterial map={texture_5} attach="material-4"/>
-            <meshStandardMaterial map={texture_6} attach="material-5"/>
+        <motion.mesh ref={mesh}>
+            <boxGeometry args={[2.5, 2.5, 2.5]} />
+            <meshStandardMaterial map={texture_1} attach="material-0" />
+            <meshStandardMaterial map={texture_2} attach="material-1" />
+            <meshStandardMaterial map={texture_3} attach="material-2" />
+            <meshStandardMaterial map={texture_4} attach="material-3" />
+            <meshStandardMaterial map={texture_5} attach="material-4" />
+            <meshStandardMaterial map={texture_6} attach="material-5" />
         </motion.mesh>
     )
 }
